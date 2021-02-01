7c25f3b1ede6b20a3a38817594ed1a5e28de2f01
I3at57 <75752042+I3at57@users.noreply.github.com>
2021-02-01 19:14:00 +0100

    Début du programme de déplacement

--- mouvturtle.py
+++ mouvturtle.py
@@ -1,18 +1,10 @@
 import turtle
 from random import randint
 
+tBleu = turtle.Turtle()
 tBleu.shape("square")
-tJaune.shape("square")
 
-turtle.mainloop()
-
-#Place le curseur tBleu en départ gauche
+# Place le curseur tBleu en départ gauche
 tBleu.penup()
-tBleu.goto(-240,40)
+tBleu.goto(-240, 40)
 tBleu.fillcolor("blue")
-
-#Place le curseur tJaune en départ gauche
-tJaune.penup()
-tJaune.goto(240,40)
-tJaune.fillcolor("yellow")
-tJaune.right(180)
