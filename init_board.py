908e694b2ef3bcb88a9fd7b8a59108917db18a96
I3at57 <75752042+I3at57@users.noreply.github.com>
2021-02-01 18:17:02 +0100

    Maj code propre

--- init_board.py
+++ init_board.py
@@ -5,51 +5,53 @@ rd = "red"
 ble = "blue"
 yl = "yellow"
 
-def dessinCercle(x,y,color):
-    turtle.goto(x,y)
+
+def dessinCercle(x, y, color):
+    turtle.goto(x, y)
     turtle.pen(pencolor=color, fillcolor=color)
     turtle.begin_fill()
     turtle.circle(5)
     turtle.end_fill()
 
+
 turtle.bgcolor("aqua")
 turtle.title("Plateau de la coupe")
 turtle.speed(100)
-turtle.shapesize(0.1,0.1,0.1)
+turtle.shapesize(0.1, 0.1, 0.1)
 
 ###############################################
 
-#dessiner le cadre
+# dessiner le cadre
 turtle.penup()
-turtle.goto(-300,200)
+turtle.goto(-300, 200)
 turtle.pendown()
-turtle.goto(300,200)
-turtle.goto(300,-200)
-turtle.goto(-300,-200)
-turtle.goto(-300,200)
+turtle.goto(300, 200)
+turtle.goto(300, -200)
+turtle.goto(-300, -200)
+turtle.goto(-300, 200)
 turtle.penup()
 
-#dessiner la zone de jeu1
-turtle.goto(-300,100)
+# dessiner la zone de jeu1
+turtle.goto(-300, 100)
 turtle.pen(pencolor="blue")
 turtle.pendown()
-turtle.goto(-220,100)
-turtle.goto(-220,-20)
-turtle.goto(-300,-20)
+turtle.goto(-220, 100)
+turtle.goto(-220, -20)
+turtle.goto(-300, -20)
 turtle.pen(pencolor="black")
 turtle.penup()
 
-#dessiner la zone de jeu2
-turtle.goto(300,100)
+# dessiner la zone de jeu2
+turtle.goto(300, 100)
 turtle.pen(pencolor="yellow")
 turtle.pendown()
-turtle.goto(220,100)
-turtle.goto(220,-20)
-turtle.goto(300,-20)
+turtle.goto(220, 100)
+turtle.goto(220, -20)
+turtle.goto(300, -20)
 turtle.pen(pencolor="black")
 turtle.penup()
 
-turtle.goto(-70,-200)
+turtle.goto(-70, -200)
 turtle.pen(pencolor="yellow")
 turtle.pendown()
 turtle.forward(20)
@@ -63,7 +65,7 @@ turtle.left(90)
 turtle.pen(pencolor="black")
 turtle.penup()
 
-turtle.goto(50,-200)
+turtle.goto(50, -200)
 turtle.pen(pencolor="blue")
 turtle.pendown()
 turtle.forward(20)
@@ -77,34 +79,34 @@ turtle.left(90)
 turtle.pen(pencolor="black")
 turtle.penup()
 
-#############################################################""
+#############################################################
 
-#dessiner les rochers
-turtle.goto(-122.2,-200)
+# dessiner les rochers
+turtle.goto(-122.2, -200)
 turtle.pendown()
-turtle.goto(-122.2,-170)
-turtle.goto(-117.8,-170)
-turtle.goto(-117.8,-200)
+turtle.goto(-122.2, -170)
+turtle.goto(-117.8, -170)
+turtle.goto(-117.8, -200)
 turtle.penup()
 
-turtle.goto(117.8,-200)
+turtle.goto(117.8, -200)
 turtle.pendown()
-turtle.goto(117.8,-170)
-turtle.goto(122.2,-170)
-turtle.goto(122.2,-200)
+turtle.goto(117.8, -170)
+turtle.goto(122.2, -170)
+turtle.goto(122.2, -200)
 turtle.penup()
 
-turtle.goto(-2.2,-200)
+turtle.goto(-2.2, -200)
 turtle.pendown()
-turtle.goto(-2.2,-140)
-turtle.goto(2.2,-140)
-turtle.goto(2.2,-200)
+turtle.goto(-2.2, -140)
+turtle.goto(2.2, -140)
+turtle.goto(2.2, -200)
 turtle.penup()
 
-######################################################""
+######################################################
 
-#Zone verte blueside
-turtle.goto(-300,94)
+# Zone verte blueside
+turtle.goto(-300, 94)
 turtle.pen(pencolor="green", fillcolor="green")
 turtle.begin_fill()
 turtle.forward(80)
@@ -117,8 +119,8 @@ turtle.forward(6)
 turtle.left(90)
 turtle.end_fill()
 
-#Zone rouge blueside
-turtle.goto(-300,-14)
+# Zone rouge blueside
+turtle.goto(-300, -14)
 turtle.pen(pencolor="red", fillcolor="red")
 turtle.begin_fill()
 turtle.forward(80)
@@ -131,8 +133,8 @@ turtle.forward(6)
 turtle.right(90)
 turtle.end_fill()
 
-#Zone rouge yellowside
-turtle.goto(220,94)
+# Zone rouge yellowside
+turtle.goto(220, 94)
 turtle.pen(pencolor="red", fillcolor="red")
 turtle.begin_fill()
 turtle.forward(80)
@@ -145,8 +147,8 @@ turtle.forward(6)
 turtle.left(90)
 turtle.end_fill()
 
-#Zone verte yellowside
-turtle.goto(220,-14)
+# Zone verte yellowside
+turtle.goto(220, -14)
 turtle.pen(pencolor="green", fillcolor="green")
 turtle.begin_fill()
 turtle.forward(80)
@@ -159,10 +161,10 @@ turtle.forward(6)
 turtle.right(90)
 turtle.end_fill()
 
-###################################################################################
+##############################################################################
 
-#Zone verte bluebotside
-turtle.goto(-90,-200)
+# Zone verte bluebotside
+turtle.goto(-90, -200)
 turtle.pen(pencolor="green", fillcolor="green")
 turtle.begin_fill()
 turtle.forward(20)
@@ -175,8 +177,8 @@ turtle.forward(60)
 turtle.left(90)
 turtle.end_fill()
 
-#Zone rouge bluebotside
-turtle.goto(-50,-200)
+# Zone rouge bluebotside
+turtle.goto(-50, -200)
 turtle.pen(pencolor="red", fillcolor="red")
 turtle.begin_fill()
 turtle.forward(20)
@@ -189,8 +191,8 @@ turtle.forward(60)
 turtle.left(90)
 turtle.end_fill()
 
-#Zone verte yellowbotside
-turtle.goto(30,-200)
+# Zone verte yellowbotside
+turtle.goto(30, -200)
 turtle.pen(pencolor="green", fillcolor="green")
 turtle.begin_fill()
 turtle.forward(20)
@@ -203,8 +205,8 @@ turtle.forward(60)
 turtle.left(90)
 turtle.end_fill()
 
-#Zone rouge yellowbotside
-turtle.goto(70,-200)
+# Zone rouge yellowbotside
+turtle.goto(70, -200)
 turtle.pen(pencolor="red", fillcolor="red")
 turtle.begin_fill()
 turtle.forward(20)
@@ -217,38 +219,38 @@ turtle.forward(60)
 turtle.left(90)
 turtle.end_fill()
 
-########################################################################################
+###############################################################################
 
-#Gobi sideblue base
-dessinCercle(-240,120,rd)
-dessinCercle(-211,97,gr)
-dessinCercle(-211,-17,rd)
-dessinCercle(-240,-40,gr)
-#Gobi sideyellow base
-dessinCercle(240,120,gr)
-dessinCercle(211,97,rd)
-dessinCercle(211,-17,gr)
-dessinCercle(240,-40,rd)
-#Gobi sideblue large
-dessinCercle(-166,180,rd)
-dessinCercle(-108.8,120,gr)
-dessinCercle(-80,40,rd)
-dessinCercle(-48,-40,gr)
-#Gobi sideyellow large
-dessinCercle(166,180,gr)
-dessinCercle(108.8,120,rd)
-dessinCercle(80,40,gr)
-dessinCercle(48,-40,rd)
-#Gobi Rochers sideyellow
-dessinCercle(-99,-191,rd)
-dessinCercle(-87,-131,gr)
-dessinCercle(-33,-131,rd)
-dessinCercle(-21,-191,gr)
-#Gobi Rochers sideblue
-dessinCercle(21,-191,rd)
-dessinCercle(33,-131,gr)
-dessinCercle(87,-131,rd)
-dessinCercle(99,-191,gr)
+# Gobi sideblue base
+dessinCercle(-240, 120, rd)
+dessinCercle(-211, 97, gr)
+dessinCercle(-211, -17, rd)
+dessinCercle(-240, -40, gr)
+# Gobi sideyellow base
+dessinCercle(240, 120, gr)
+dessinCercle(211, 97, rd)
+dessinCercle(211, -17, gr)
+dessinCercle(240, -40, rd)
+# Gobi sideblue large
+dessinCercle(-166, 180, rd)
+dessinCercle(-108.8, 120, gr)
+dessinCercle(-80, 40, rd)
+dessinCercle(-48, -40, gr)
+# Gobi sideyellow large
+dessinCercle(166, 180, gr)
+dessinCercle(108.8, 120, rd)
+dessinCercle(80, 40, gr)
+dessinCercle(48, -40, rd)
+# Gobi Rochers sideyellow
+dessinCercle(-99, -191, rd)
+dessinCercle(-87, -131, gr)
+dessinCercle(-33, -131, rd)
+dessinCercle(-21, -191, gr)
+# Gobi Rochers sideblue
+dessinCercle(21, -191, rd)
+dessinCercle(33, -131, gr)
+dessinCercle(87, -131, rd)
+dessinCercle(99, -191, gr)
 ###############################################################
 turtle.home()
 turtle.mainloop()
