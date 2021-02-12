a7ec2a097e7dca5d8b4f6d255d904ad9e853c340
I3at57 <75752042+I3at57@users.noreply.github.com>
2021-02-12 17:38:33 +0100

    création de la zone de test

---
+++ testenvrac.py
@@ -0,0 +1,12 @@
+import turtle
+import constantes
+import robot_mouvment
+import init_board
+
+turtle.colormode(0xFF)
+turtle.speed(100)
+turtle.shapesize(0.1, 0.1, 0.1)
+turtle.penup()
+init_board.dessin_Gobies_init2()
+
+turtle.mainloop()
