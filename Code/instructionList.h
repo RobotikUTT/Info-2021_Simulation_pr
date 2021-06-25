0131d3186f10488235a11a9e17603f9e5070915a
I3at57 <75752042+I3at57@users.noreply.github.com>
2021-06-25 13:43:32 +0200

    test

--- Code/instructionList.h
+++ Code/instructionList.h
@@ -7,7 +7,7 @@
 #ifndef INSTRUCTIONLIST_H
 #define INSTRUCTIONLIST_H
 
-int golist = [1000, 200];
-int nbrGoto = 2;
+int golist = [300, 400, 1270, 1200];
+int nbrGoto = 4;
 
 #endif // INSTRUCTIONLIST_H
